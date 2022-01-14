5fa64316123aa0ac5cdeb8d25b267d4c97b80d98
kennedy bernard <makuanikennedy@gmail.com>
2022-01-14 14:12:35 +0300

    added profile folder

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import {GithubService} from 'AppComponent';
+import 'rxjs/add/operator/map';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+  user: any= [];
+  repos: any= [];
+  username!: string;
+  constructor(private _githubService: GithubService) {
+    this._githubService.getUser().subscribe((user: any) => {
+      console.log(user);
+      this.user = user;
+    });
+    this._githubService.getRepos().subscribe((repos: any) => {
+      // console.log(user);
+      this.repos = repos;
+    });
+  }
+  ngOnInit() {
+  }
+   searchUser() {
+     this._githubService.updateUser(this.username);
+     this._githubService.getUser().subscribe((user: any) => {
+       this.user = user;
+     });
+     // Console.log('It works');
+     this._githubService.getRepos().subscribe((repos: any) => {
+       // Console.log(user);
+       this.repos = repos;
+     });
+
+   }
+}
