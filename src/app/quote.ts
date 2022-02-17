114646dcc5710af04546206a94502780643500ad
kennedy bernard <makuanikennedy@gmail.com>
2022-02-17 20:57:09 +0300

    Auto-generated commit [ci skip]

--- src/app/quote.ts
+++
@@ -1,6 +0,0 @@
-export class Quote {
-  showDescription: boolean;
-  constructor(public id: number, public quote: string, public author: string, public postedBy: string, public upvotes: number, public downvotes: number, public dayAdded: Date) {
-    this.showDescription = false;
-  }
-}
